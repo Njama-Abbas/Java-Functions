8024a7362f7b67c3121b9f0b0710cb48f02fd0e8
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-09 16:28:06 +0300

    Functions Definition and Usage of Functions

--- src/com/javafunctions/Main.java
+++ src/com/javafunctions/Main.java
@@ -1,23 +1,31 @@
 package com.javafunctions;
 
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
 public class Main {
 
+public  static  void announceTeaTime(){
+    //these are the lines of code to be executed when the function is used
+    System.out.println("Awaiting for the announcement......");
+    System.out.println("Type in a random word and press enter to start the developer tea-time ");
+    Scanner scanner = new Scanner(System.in);
+    scanner.next();
+    System.out.println("It is developer Tea Time");
+}
     public static void main(String[] args) {
-	// write your code here
-        /*
-        code so far runs all sequentially
-        program design is also important more than program design
-
-        Functions
-        A function is A series of finite steps that accomplish some task
-        Functions have a name (action like names)
-        each time you need to perform the task
-        all you need is to use the name of the function
-        with additional opening and closing parenthesis
-
-        Functions help developers organize their code in a more meaningful way
-        Allows definition of a given task once and usage through out the code
-
+	/*
+        differentiate between definition and usage
          */
+        System.out.println("Welcome to your new Job");
+
+        announceTeaTime();
+
+        System.out.println("Write code");
+        System.out.println("Review Code");
+
+        announceTeaTime();
+        System.out.println("Get Promoted");
+
     }
 }
