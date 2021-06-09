a7e594410849d250845150d88ac84d0d6893904b
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-09 16:53:09 +0300

    Functions Parameters. Arguments and Return Types

--- src/com/javafunctions/Main.java
+++ src/com/javafunctions/Main.java
@@ -14,14 +14,36 @@ public class Main {
          */
 
 
-    public static void calculateTotalMealPrice() {
+    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
+    double tip = tipRate * listedMealPrice;
+    double tax = taxRate * listedMealPrice;
+    double result = listedMealPrice + tip +tax;
+        System.out.println("Your total meal Price  "  + result);
+    }
 
+    public static double calculateTotalMealPriceTwo(double listedMealPrice, double tipRate, double taxRate) {
+        double tip = tipRate * listedMealPrice;
+        double tax = taxRate * listedMealPrice;
+        double result = listedMealPrice + tip +tax;
+          /*
+    split the bill
+    return statement allows to take a value of a function and make it available at a different scope
+     */
+       return  result;
     }
 
     public static void main(String[] args) {
-
         System.out.println("Welcome to Java House");
+        calculateTotalMealPrice(100, 0.25, 0.8);
+        calculateTotalMealPrice(200, 0.15, 0.7);
 
 
+        double totalMealPrice = calculateTotalMealPriceTwo(100, 0.24, 0.8);
+        System.out.println("Group Meal Price " + totalMealPrice);
+        double  individualMealPrice = totalMealPrice/4;
+        System.out.println("Individual Meal Price " + individualMealPrice);
     }
+
+
+
 }
