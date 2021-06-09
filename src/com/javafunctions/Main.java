6dadac7fe87736f060f798ed8eab8568f93680b3
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-09 16:10:13 +0300

    Functions Understanding functions in Java

---
+++ src/com/javafunctions/Main.java
@@ -0,0 +1,23 @@
+package com.javafunctions;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+        /*
+        code so far runs all sequentially
+        program design is also important more than program design
+
+        Functions
+        A function is A series of finite steps that accomplish some task
+        Functions have a name (action like names)
+        each time you need to perform the task
+        all you need is to use the name of the function
+        with additional opening and closing parenthesis
+
+        Functions help developers organize their code in a more meaningful way
+        Allows definition of a given task once and usage through out the code
+
+         */
+    }
+}
