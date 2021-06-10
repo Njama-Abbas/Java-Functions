cd1d5c3f76d35e154a35ce2072335131ae485971
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-10 16:23:09 +0300

    Functions Interacting With Built In Functions

--- src/com/javafunctions/Main.java
+++ src/com/javafunctions/Main.java
@@ -4,44 +4,19 @@ import java.sql.SQLOutput;
 import java.util.Scanner;
 
 public class Main {
-    /*
-        A function that accepts inputs
-        Having a dynamic output depending on what was inputted in a function
-        Write a function to calculate the total cost of a meal
-        --Tip = tipRate * listedMealPrice
-        --tax = taxRate * listedMealPrice
-        --total cost = tip +tax listedMealPrice
-         */
-
-
-    public static void calculateTotalMealPrice(double listedMealPrice, double tipRate, double taxRate) {
-    double tip = tipRate * listedMealPrice;
-    double tax = taxRate * listedMealPrice;
-    double result = listedMealPrice + tip +tax;
-        System.out.println("Your total meal Price  "  + result);
-    }
-
-    public static double calculateTotalMealPriceTwo(double listedMealPrice, double tipRate, double taxRate) {
-        double tip = tipRate * listedMealPrice;
-        double tax = taxRate * listedMealPrice;
-        double result = listedMealPrice + tip +tax;
-          /*
-    split the bill
-    return statement allows to take a value of a function and make it available at a different scope
-     */
-       return  result;
-    }
 
     public static void main(String[] args) {
-        System.out.println("Welcome to Java House");
-        calculateTotalMealPrice(100, 0.25, 0.8);
-        calculateTotalMealPrice(200, 0.15, 0.7);
+        /*
+         * Functions created so far have been user defined [Software developer made]
+         * Java has inbuilt functions that developers can use instead of defining there own each time
+         * To use inbuilt functions you just have to call the function by its name because its already defined
+         * System.out.println()  is an inbuilt function [Never defined but is usable]
+         * "red".equal("blue")
+         * use the dot operator for accessing many of the built-in functions
+         * as a developer sometimes you will define your own custom functions and
+         * sometimes you will use builtin functions
+         */
 
-
-        double totalMealPrice = calculateTotalMealPriceTwo(100, 0.24, 0.8);
-        System.out.println("Group Meal Price " + totalMealPrice);
-        double  individualMealPrice = totalMealPrice/4;
-        System.out.println("Individual Meal Price " + individualMealPrice);
     }
 
 
