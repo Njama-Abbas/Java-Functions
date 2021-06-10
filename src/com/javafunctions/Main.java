e7f8a47c686e092408292317ec6ae36a482694aa
Baraka Mulumia <barakamulumia@gmail.com>
2021-06-10 16:29:22 +0300

    Functions Consuming the Math Inbuilt Methods

--- src/com/javafunctions/Main.java
+++ src/com/javafunctions/Main.java
@@ -1,24 +1,19 @@
 package com.javafunctions;
 
-import java.sql.SQLOutput;
-import java.util.Scanner;
+;
 
 public class Main {
 
     public static void main(String[] args) {
         /*
-         * Functions created so far have been user defined [Software developer made]
-         * Java has inbuilt functions that developers can use instead of defining there own each time
-         * To use inbuilt functions you just have to call the function by its name because its already defined
-         * System.out.println()  is an inbuilt function [Never defined but is usable]
-         * "red".equal("blue")
-         * use the dot operator for accessing many of the built-in functions
-         * as a developer sometimes you will define your own custom functions and
-         * sometimes you will use builtin functions
+        sample exponential function
+        all you have to know is the functions inputs and output
+        if there is a built-in functionality for what you want to implement
+         use it instead of having to define your own
          */
+        System.out.println(Math.pow(4, 5));
 
     }
-
 
 
 }
